f75ebad699143cba15eab604ff25af82bfdd1d14
Nohan JAUGEY <nohan.jaugey@etu.unistra.fr>
2021-02-16 21:45:06 +0100

    Mobile

---
+++ mobile/app/src/main/java/site/nohan/protoprogression/Model/Map.java
@@ -0,0 +1,33 @@
+package site.nohan.protoprogression.Model;
+
+import android.graphics.Point;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Map {
+    public static int accompli;
+
+    public static Chemin cheminActuel;
+
+    public static ArrayList<Chemin> chemins;
+
+
+    static{
+        accompli = 0;
+
+        Chemin chemin = new Chemin();
+        chemin.points = new ArrayList<>();
+        chemin.points.add(new Point(20,20));
+        chemin.points.add(new Point(50,50));
+        chemin.points.add(new Point(60,60));
+        chemin.points.add(new Point(80,90));
+
+        Map.chemins = new ArrayList<>();
+        cheminActuel = chemin;
+        Map.chemins.add(chemin);
+        Log.e("static", chemin.getLongueurAt(chemin.points.get(1))+"");
+    }
+
+
+}
---
+++ mobile/app/src/main/java/site/nohan/protoprogression/Network/Map/MapRequest.java
@@ -0,0 +1,24 @@
+package site.nohan.protoprogression.Network.Map;
+
+import android.app.Activity;
+import android.util.Log;
+
+import site.nohan.protoprogression.Model.Map;
+import site.nohan.protoprogression.Network.APIRequest;
+
+public class MapRequest extends APIRequest {
+    static String mapURL = "challenge/";
+
+    private Map map;
+
+
+    public MapRequest(Activity activity,  int mapId) {
+        super(activity, "challenge/"+mapId, new MapResponse());
+        Log.e("net", this.getUrl());
+        APIRequest.queue.add(this);
+        APIRequest.queue.start();
+    }
+
+
+
+}
