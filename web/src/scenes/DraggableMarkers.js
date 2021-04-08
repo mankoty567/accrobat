3e46f28eccef9ff546d23c9a54f09b9947a371d4
Xavier Schuszter <schuszter@etu.unistra.fr>
2021-04-08 09:25:50 +0200

    Merge conflict !

--- web/src/scenes/DraggableMarkers.js
+++ web/src/scenes/DraggableMarkers.js
@@ -15,7 +15,6 @@ import API from '../eventApi/eventApi';
  * @param {Object[]} lines La liste des lignes à afficher sur la map
  * @param {Function} setLines Fonction pour update le state de lines
  */
-<<<<<<< HEAD
 let DraggableMarkers = ({
   markers,
   setMarkers,
@@ -26,61 +25,42 @@ let DraggableMarkers = ({
   currentMarker,
   setCurrentMarker,
   setStartPoint,
-  startPoint,
+  currentLine,
+  setCurrentLine,
 }) => {
-=======
-let DraggableMarkers = ({ markers, setMarkers, editMode, setEditMode, lines, setLines, currentMarker, setCurrentMarker, setStartPoint, currentLine, setCurrentLine}) => {
-
->>>>>>> 39f18c7479a7eee07e3380a7df69878814da7c14
   //Ajoute un marker
   let addMarker = async (event) => {
     var id = markers.length > 0 ? markers.slice(-1)[0].id + 1 : 0;
     var newMarker = {
-<<<<<<< HEAD
+      id: id,
       title: 'Point ' + id,
       description: '',
       type: markers.length > 0 ? 'point' : 'start',
-      x: event.latlng.lat,
-      y: event.latlng.lng,
-      frontId: id,
-    };
-    return API.createMarker(newMarker).then((res) => {
-      newMarker.id = res.id;
-      setMarkers((current) => [...current, res]);
-      setStartPoint(newMarker);
-      return newMarker;
-    });
-=======
-      'id': id,
-      'title': 'Point ' + id,
-      'description': '',
-      'type': markers.length > 0 ? 'point' : 'start',
-      'x': event.latlng.lng,
-      'y': event.latlng.lat
+      x: event.latlng.lng,
+      y: event.latlng.lat,
     };
     setMarkers((current) => [...current, newMarker]);
     setStartPoint(newMarker);
     setCurrentMarker(newMarker);
     return newMarker;
->>>>>>> 39f18c7479a7eee07e3380a7df69878814da7c14
   };
 
   let inBounds = (event) => {
-    return!(
+    return !(
       event.latlng.lat < 0 ||
       event.latlng.lat > 1 ||
       event.latlng.lng < 0 ||
       event.latlng.lng > 1
     );
-  }
+  };
 
   //Ajoute une ligne
   let addLine = (start, end) => {
     var newLines = {
-      'id': lines.length > 0 ? lines.slice(-1)[0].id + 1 : 0,
-      'PointStartId': start.id,
-      'PointEndId': end.id,
-      'path': currentLine
+      id: lines.length > 0 ? lines.slice(-1)[0].id + 1 : 0,
+      PointStartId: start.id,
+      PointEndId: end.id,
+      path: currentLine,
     };
     return API.createSegment(newLines).then((res) => {
       newLines.id = res.id;
@@ -94,7 +74,7 @@ let DraggableMarkers = ({ markers, setMarkers, editMode, setEditMode, lines, set
     } else {
       setCurrentLine((current) => [...current, newPoint.latlng]);
     }
-  }
+  };
 
   //Récupère l'icône en fonction du type du marker
   let getIcon = (marker) => {
@@ -145,36 +125,37 @@ let DraggableMarkers = ({ markers, setMarkers, editMode, setEditMode, lines, set
                 icon={getIcon(item)}
                 eventHandlers={{
                   click: () => {
-<<<<<<< HEAD
-                    {
-                      currentMarker
-                        ? currentMarker.id == item.id
-                          ? setCurrentMarker(null)
-                          : setCurrentMarker(item)
-                        : setCurrentMarker(item);
-                    }
-                    if (editMode && item.type != 'start') {
-=======
                     // {currentMarker ? (currentMarker.id === item.id ? (editMode ? null : setCurrentMarker(null)) : setCurrentMarker(item)) : setCurrentMarker(item)}
                     var newCurrent = item;
                     if (currentMarker) {
-                      if (currentMarker.id === item.id && editMode) newCurrent = null;
+                      if (currentMarker.id === item.id && editMode)
+                        newCurrent = null;
                     }
                     setCurrentMarker(newCurrent);
-                    if(editMode && item.type != 'start' && currentMarker.id != item.id) {
->>>>>>> 39f18c7479a7eee07e3380a7df69878814da7c14
+                    if (
+                      editMode &&
+                      item.type != 'start' &&
+                      currentMarker.id != item.id
+                    ) {
                       setEditMode(false);
                       addLine(currentMarker, item);
                       // setStartPoint(item);
                     }
                   },
-<<<<<<< HEAD
-=======
                   dragend: (event) => {
-                    setMarkers(markers => markers.map(m => m.id === item.id ? { ...m, x: event.target._latlng.lng, y: event.target._latlng.lat } : m));
+                    setMarkers((markers) =>
+                      markers.map((m) =>
+                        m.id === item.id
+                          ? {
+                              ...m,
+                              x: event.target._latlng.lng,
+                              y: event.target._latlng.lat,
+                            }
+                          : m,
+                      ),
+                    );
                     setEditMode(false);
-                  }
->>>>>>> 39f18c7479a7eee07e3380a7df69878814da7c14
+                  },
                 }}
               >
                 <Tooltip direction="top" offset={[0, -40]} permanent>
