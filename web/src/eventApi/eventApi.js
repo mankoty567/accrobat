57bae6a10dca5c59fc660767a01443ee6dfa18c8
Jules <bigaston1@gmail.com>
2021-04-08 10:32:26 +0200

    Suppression de marker

--- web/src/eventApi/eventApi.js
+++ web/src/eventApi/eventApi.js
@@ -86,11 +86,11 @@ let checkStatus = (res) => {
             body: JSON.stringify(data.marker)
         }).then(checkStatus).then(res => res.json());
     },
-    deleteMarker: (data) => {
-        return fetch(`${host}/api/pointpassage/${data.marker_id}`, {
+    deleteMarker: (marker) => {
+        return fetch(`${host}/api/pointpassage/${marker.id}`, {
             method: 'DELETE',
             headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem('token') }
-        }).then(checkStatus).then(res => res.json());
+        }).then(checkStatus).then(res => res.text());
     },
 
     getSegment: () => {
@@ -123,7 +123,7 @@ let checkStatus = (res) => {
         return fetch(`${host}/api/segment/${data.segment_id}`, {
             method: 'DELETE',
             headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem('token') }
-        }).then(checkStatus).then(res => res.json());
+        }).then(checkStatus).then(res => res.text());
     },
 
     createObstacle: (data) => {
@@ -153,7 +153,7 @@ let checkStatus = (res) => {
                 'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
                 'Content-Type': 'application/json'
             }
-        }).then(checkStatus).then(res => res.json());
+        }).then(checkStatus).then(res => res.text());
     },
 
     getParticipations: (data) => {
