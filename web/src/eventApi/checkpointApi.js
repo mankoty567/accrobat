d3c0cc361399abbb2f202cc8bf81f46904a59334
Xavier Schuszter <schuszter@etu.unistra.fr>
2021-04-15 11:29:27 +0200

    [WEB]Refactor de l'eventApi

---
+++ web/src/eventApi/checkpointApi.js
@@ -0,0 +1,82 @@
+import { checkStatus, host } from './api';
+
+export default checkpointApi = {
+  /**
+   * Permet d'obtenir tout les points de passage d'un challenge
+   * @param {Object} data
+   * @param {String} data.challenge_id L'id du challenge
+   * @returns Une liste de markers
+   */
+  getMarkers: (data) => {
+    return fetch(
+      `${host}/api/challenge/${data.challenge_id}/point?include=${data.include}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization:
+            'Bearer ' + window.localStorage.getItem('token'),
+        },
+      },
+    )
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+
+  /**
+   * Permet de créer un point de passage à partir d'un id de challenge
+   * @param {Object} data
+   * @param {String} data.challenge_id L'id du challenge
+   * @returns Un objet point de passage
+   */
+  createMarker: (data) => {
+    return fetch(`${host}/api/challenge/${data.challenge_id}/point`, {
+      method: 'POST',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data.marker),
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+
+  /**
+   * Permet d'update les informations d'un point de passage à partir de son id
+   * @param {Object} data
+   * @param {String} data.marker_id L'id du point de passage
+   * @returns Un objet point de passage
+   */
+  updateMarker: (data) => {
+    return fetch(`${host}/api/pointpassage/${data.marker_id}`, {
+      method: 'POST',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data.marker),
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+
+  /**
+   * Permet de supprimer un point de passage à partir de son id
+   * @param {Object} data
+   * @param {String} data.marker_id L'id du point de passage
+   * @returns Un message au status 200 ou 400
+   */
+  deleteMarker: (data) => {
+    return fetch(`${host}/api/pointpassage/${data.marker_id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+      },
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+};
---
+++ web/src/eventApi/obstacleApi.js
@@ -0,0 +1,75 @@
+import { checkStatus, host } from './api';
+
+export default obstacleApi = {
+  /**
+   * Permet de créer un obstacle
+   * @param {Object} data
+   * @param {String} [data.enigme_awnser] Réponse attendue de l'énigme. Optionnelle il peut s'agir d'un challenge par image
+   * @param {Number} [data.frontId] Id en front
+   * @param {String} data.title Titre de l'obstacle
+   * @param {String} data.description Description de l'obstacle
+   * @param {String} data.type Type de l'obstacle (image ou enigme)
+   * @param {Number} data.distance Distance du segment sur laquelle l'obstacle se trouve
+   * @param {Number} data.SegmentId Id du segment attaché
+   * @param {String} data.enigme_img Image de l'enigme, encodée en base64
+   * @returns L'objet obstacle créé
+   */
+  createObstacle: (data) => {
+    return fetch(`${host}/api/obstacle`, {
+      method: 'POST',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data.obstacle),
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+
+  /**
+   * Permet de modifier les informations d'un obstacle
+   * @param {Object} data
+   * @param {String} [data.title] Titre de l'obstacle
+   * @param {String} [data.description] Description de l'obstacle
+   * @param {String} [data.type] Type de l'obstacle (image ou enigme)
+   * @param {Number} [data.distance] Distance du segment sur laquelle l'obstacle se trouve
+   * @param {Number} data.obstacle_id id de l'obstacle en question
+   * @param {String} [data.enigme_img] Image de l'enigme, encodée en base64
+   * @param {String} [data.enigme_awnser] Réponse attendue de l'énigme. Optionnelle il peut s'agir d'un challenge par image
+   * @returns
+   */
+  updateObstacle: (data) => {
+    return fetch(`${host}/api/obstacle/${data.obstacle_id}`, {
+      method: 'POST',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data.obstacle),
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+
+  /**
+   * Permet de supprimer un obstacle depuis son id
+   * @param {Object} data
+   * @param {String} data.obstacle_id id de l'obstacle
+   * @returns
+   */
+  deleteObstacle: (data) => {
+    return fetch(`${host}/api/obstacle/${data.obstacle_id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization:
+          'Bearer ' + window.localStorage.getItem('token'),
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+};
