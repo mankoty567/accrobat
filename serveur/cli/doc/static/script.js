1f9339bd180a931baf043de985be04e6e111c654
Jules <bigaston1@gmail.com>
2021-05-20 10:14:59 +0200

    Ajout d'un bouton pour copier l'url

--- serveur/cli/doc/static/script.js
+++ serveur/cli/doc/static/script.js
@@ -10,6 +10,8 @@ document.querySelectorAll('.categorie h2').forEach((cate) => {
 
 document.querySelectorAll('.route h3').forEach((cate) => {
   cate.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') return;
+
     e.target.parentNode
       .querySelector('.routeContent')
       .classList.toggle('hidden');
@@ -21,3 +23,28 @@ bddTitle.addEventListener('click', () => {
   document.querySelector('.bdd').classList.toggle('hidden');
   bddTitle.classList.toggle('opened');
 });
+
+document.querySelectorAll('.copyIcon').forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    copyToClipboard(e.target.getAttribute('urlValue'));
+    // eslint-disable-next-line no-undef
+    Toastify({
+      text: 'URL Copiée dans le Presse Papier',
+      duration: 3000,
+      close: true,
+      gravity: 'bottom', // `top` or `bottom`
+      position: 'center', // `left`, `center` or `right`
+      backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+    }).showToast();
+  });
+});
+
+const copyToClipboard = (str) => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
