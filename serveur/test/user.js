23ef88cc023d85fec30e6421480e5ebb5676a72c
Jules <bigaston1@gmail.com>
2021-05-28 10:04:59 +0200

    Ajout du report

--- serveur/test/user.js
+++ serveur/test/user.js
@@ -21,5 +21,41 @@ describe('User', function () {
           throw err;
         });
     });
+
+    it('Username déjà existant', (done) => {
+      chai
+        .request(app)
+        .post('/api/user/register')
+        .send({
+          username: 'admin',
+          password: '123456',
+          email: 'admin@admin.com',
+        })
+        .then(function (res) {
+          expect(res.status).to.equal(400);
+          done();
+        })
+        .catch(function (err) {
+          throw err;
+        });
+    });
+
+    it('Bon utilisateur', (done) => {
+      chai
+        .request(app)
+        .post('/api/user/register')
+        .send({
+          username: 'user',
+          password: '123456',
+          email: 'user@user.com',
+        })
+        .then(function (res) {
+          expect(res.status).to.equal(200);
+          done();
+        })
+        .catch(function (err) {
+          throw err;
+        });
+    });
   });
 });
