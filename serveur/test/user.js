76e81ae90fff8d271c7c7489b21b6d0f0496734d
Jules <bigaston1@gmail.com>
2021-05-28 12:19:06 +0200

    Correction timout

--- serveur/test/user.js
+++ serveur/test/user.js
@@ -1,5 +1,7 @@
 const { app } = require('../serveur');
 
+const global = require('./hooks').global;
+
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 
@@ -51,10 +53,60 @@ describe('User', function () {
         })
         .then(function (res) {
           expect(res.status).to.equal(200);
+
           done();
         })
         .catch(function (err) {
           throw err;
+        });
+    });
+  });
+
+  describe('POST /api/user/login', () => {
+    it("Connecter l'utilisateur", (done) => {
+      chai
+        .request(app)
+        .post('/api/user/login')
+        .send({
+          username: 'user',
+          password: '123456',
+        })
+        .then(function (res) {
+          expect(res.status).to.equal(200);
+
+          expect(res.body).to.have.property('username').with.equal('user');
+          expect(res.body)
+            .to.have.property('email')
+            .with.equal('user@user.com');
+          expect(res.body).to.have.property('permission').with.equal(0);
+          expect(res.body).to.have.property('jwt');
+
+          global.jwt = res.body.jwt;
+
+          done();
+        })
+        .catch(function (err) {
+          throw err;
+        });
+    });
+  });
+
+  describe('POST /api/user/whoami', () => {
+    it('Passe un JWT Valide', (done) => {
+      if (global.jwt === undefined) {
+        this.skip();
+        return;
+      }
+
+      chai
+        .request(app)
+        .get('/api/user/whoami')
+        .set('Authorization', 'Bearer ' + global.jwt)
+        .then((res) => {
+          expect(res.status).to.equal(200);
+
+          console.log(res.status);
+          console.log(res.body);
         });
     });
   });
